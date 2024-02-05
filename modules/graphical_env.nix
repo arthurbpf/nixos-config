b9e092f9991921c080d26d33d43eab84c8f8c096
Arthur Fernandes <arthurbpfernandes@gmail.com>
2024-02-05 09:55:42 -0300

    fix: add env vars for nvidia gpu

--- modules/graphical_env.nix
+++ modules/graphical_env.nix
@@ -59,6 +59,12 @@
     xwayland.enable = true;
   };
 
+  environment.sessionVariables = {
+    WLR_NO_HARDWARE_CURSORS = "1";
+    NIXOS_OZONE_WL = "1";
+  };
+
+
   security.pam.services.swaylock = {
     text = ''
       auth include login
