9f7105f79441b2cad300e86421be383ca1227ab1
Arthur Fernandes <arthurbpfernandes@gmail.com>
2024-03-06 21:39:36 -0300

    flake update

--- modules/server.nix
+++ modules/server.nix
@@ -1,8 +1,19 @@
-{ ... }:
+{ config, ... }:
 {
   networking.firewall = {
     enable = true;
     allowedTCPPorts = [ 32400 ];
     allowedUDPPorts = [ 32400 ];
+  };
+
+  services.cloudflared = {
+    enable = false;
+  };
+
+  services.terraria = {
+    enable = false;
+    port = 7777;
+    maxPlayers = 32;
+    dataDir = config.users.users.server.home + "/terraria";
   };
 }
