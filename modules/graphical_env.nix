5493b7a28db206302302cffd9f665cf995aaed4c
Arthur Fernandes <arthurbpfernandes@gmail.com>
2024-03-15 08:42:10 -0300

    going back to X11

--- modules/graphical_env.nix
+++ modules/graphical_env.nix
@@ -26,6 +26,7 @@
     pinta
     libreoffice
 
+    /*
     nwg-look
     waybar
     libappindicator
@@ -43,6 +44,7 @@
     hyprlock
     grim
     slurp
+    */
 
     pywal
     wpgtk
@@ -51,8 +53,14 @@
     ungoogled-chromium
 
     udiskie
+
+    rofi
+    picom
+    nitrogen
+    arandr
   ];
 
+  /*
   programs.hyprland = {
     enable = true;
     package = inputs.hyprland.packages.${pkgs.system}.hyprland;
@@ -60,14 +68,6 @@
   };
 
   programs.xwayland.enable = true;
-
-  xdg.portal = {
-    enable = true;
-    wlr.enable = true;
-    extraPortals = [
-      pkgs.unstable.xdg-desktop-portal-gtk
-    ];
-  };
 
   environment.sessionVariables = {
     XDG_SESSION_TYPE = "wayland";
@@ -78,6 +78,27 @@
       auth include login
     '';
   };
+  */
+
+  xdg.portal = {
+    enable = true;
+    wlr.enable = true;
+    extraPortals = [
+      pkgs.xdg-desktop-portal-gtk
+    ];
+  };
+
+  services.xserver = {
+    enable = true;
+    windowManager.i3.enable = true;
+    displayManager.startx.enable = true;
+  };
+
+  programs.xss-lock = {
+    enable = true;
+  };
+  programs.i3lock.enable = true;
+  programs.dconf.enable = true;
 
   programs.firefox = {
     enable = true;
